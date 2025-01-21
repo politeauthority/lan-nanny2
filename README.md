47385ec111b8a721dd6ebabedf959e111edeaf53
Alix Fullerton <fullertonalix0@gmail.com>
2025-01-20 18:16:47 -0700

    Build new

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Lan Nanny v0.0.0.49
+# Lan Nanny v0.0.0.50
 
 ![Build Main](https://github.com/politeauthority/lan-nanny2/actions/workflows/push-main.yaml/badge.svg)
 
--- src/lan_nanny/cli/__init__.py
+++ src/lan_nanny/cli/__init__.py
@@ -2,13 +2,23 @@
  Lan Nanny - CLI
 
 """
+from lan_nanny.api.version import version
+from lan_nanny.client import LanNannyClient
 
 
 class Cli:
 
-    def run(self):
-        print("Starting Lan Nanny CLI")
+    def __init__(self):
+        self.api_client = LanNannyClient()
+        self.options = {}
 
+    def run(self):
+        print("Starting Lan Nanny CLI: v%s" % version)
+        self.who_am_i()
+        self.options = self.api_client.get_options()
+
+    def who_am_i(self):
+        self.api_client.get_whoami()
 
 if __name__ == "__main__":
     Cli().run()
